3c69dc2b361b6cd69aa5536268c32466d8ded2eb
noahmobes <noahmobes@outlook.com>
2026-07-27 17:31:05 -0600

    Added pause menu

---
+++ Assets/Scripts/Menu/GUIController.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class GameGuiController : MonoBehaviour
+{
+    [Header("Scene Manager")]
+    public SceneLoader sceneManager;
+    [Header("Pause Menu")]
+    public GameObject settingsMenu;
+    [Header("Hud")]
+    public GameObject hud;
+    private InputAction openSettingsAction;
+
+    private void Start()
+    {
+        settingsMenu.SetActive(false);
+        hud.SetActive(true);
+        Reset();
+    }
+
+    private void Awake()
+    {
+        openSettingsAction = InputSystem.actions.FindAction("Pause");
+ 
+        if (openSettingsAction != null)
+        {
+            openSettingsAction.performed += OnOpenSettings;
+        }
+    }
+
+    private void OnEnable()
+    {
+        InputSystem.actions.Enable();
+    }
+
+    private void OnOpenSettings(InputAction.CallbackContext ctx)
+    {
+        OpenSettings();
+    }
+
+    private void OpenSettings()
+    {
+        PauseGame();
+        settingsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        ResumeGame();
+    }
+
+    public void LoadStartScene()
+    {
+        Reset();
+        sceneManager.LoadStartScene();
+    }
+
+    public void QuitGame()
+    {
+        sceneManager.QuitGame();
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        hud.SetActive(false);
+    }
+
+    private void ResumeGame()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        Time.timeScale = 1;
+        settingsMenu.SetActive(false);
+        hud.SetActive(true);
+    }
+}
