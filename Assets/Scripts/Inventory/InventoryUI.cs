275bf0f710cf7b63cf1b993c47b649e1eefc29c8
noahmobes <noahmobes@outlook.com>
2026-07-27 15:17:11 -0600

    Added Key sprite for Inventory bar; added key count on inverntory bar; commented out previous inventory item display code

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private Transform itemContainer;
-    [SerializeField] private TextMeshProUGUI itemTextPrefab;
-    [SerializeField] private TextMeshProUGUI equippedItemText;
+    // [SerializeField] private TextMeshProUGUI itemTextPrefab;
+    // [SerializeField] private TextMeshProUGUI equippedItemText;
+    [SerializeField] private TextMeshProUGUI keyNumText;
  
     private void Update()
     {
@@ -24,38 +25,40 @@ public class InventoryUI : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
+
+        keyNumText.text = Managers.Inventory.GetItemCount("key").ToString();
  
-        List<string> itemList = Managers.Inventory.GetItemList();
+        // List<string> itemList = Managers.Inventory.GetItemList();
  
-        if (itemList.Count == 0)
-        {
-            TextMeshProUGUI emptyText =
-                Instantiate(itemTextPrefab, itemContainer);
+        // if (itemList.Count == 0)
+        // {
+        //     TextMeshProUGUI emptyText =
+        //         Instantiate(itemTextPrefab, itemContainer);
  
-            emptyText.text = "No Items";
-        }
-        else
-        {
-            foreach (string item in itemList)
-            {
-                int count = Managers.Inventory.GetItemCount(item);
+        //     emptyText.text = "No Items";
+        // }
+        // else
+        // {
+        //     foreach (string item in itemList)
+        //     {
+        //         int count = Managers.Inventory.GetItemCount(item);
  
-                TextMeshProUGUI itemText =
-                    Instantiate(itemTextPrefab, itemContainer);
+        //         TextMeshProUGUI itemText =
+        //             Instantiate(itemTextPrefab, itemContainer);
  
-                itemText.text = $"{item} ({count})";
-            }
-        }
+        //         itemText.text = $"{item} ({count})";
+        //     }
+        // }
  
-        string equipped = Managers.Inventory.EquippedItem;
+        // string equipped = Managers.Inventory.EquippedItem;
  
-        if (string.IsNullOrEmpty(equipped))
-        {
-            equippedItemText.text = "";
-        }
-        else
-        {
-            equippedItemText.text = $"Equipped: {equipped}";
-        }
+        // if (string.IsNullOrEmpty(equipped))
+        // {
+        //     equippedItemText.text = "";
+        // }
+        // else
+        // {
+        //     equippedItemText.text = $"Equipped: {equipped}";
+        // }
     }
 }
