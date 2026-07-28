27ad9a2ca09eebfd83a8e215bf8a2ad6b521772c
noahmobes <noahmobes@outlook.com>
2026-07-27 18:41:34 -0600

    Added code, not working properly

--- Assets/Scripts/Interactions/LockedDoor.cs
+++ Assets/Scripts/Interactions/LockedDoor.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 public class LockedDoor :
     MonoBehaviour,
@@ -10,20 +11,45 @@ public class LockedDoor :
     private float openSpeed = 2f;
     [SerializeField]
     private float moveSpeed = 2f;
+    [SerializeField]
+    private GameObject requiredItem;
+    [SerializeField]
+    private int requiredItemCount = 1;
+    [SerializeField]
+    private bool consumeRequiredItem = true;
     private Vector3 openOffset = new Vector3(0f, 3f, 0f);
     private bool isOpen;
     private bool isMoving;
     private bool isLocked = true;
     public void Interact()
     {      
-        // Will not trigger if the door is already moving or if the player does not have a key in their inventory
-        if (isMoving || (!Managers.Inventory.HasItem("key") && isLocked))
+        // Will not trigger if the door is already moving or if the player does not have the required item in their inventory
+        if (isMoving || (!Managers.Inventory.HasItem(requiredItem.gameObject.name) && isLocked))
         {
             return;
         }
-        StartCoroutine(ToggleDoor());
-        Managers.Inventory.ConsumeItem("key");
-        isLocked = false;
+
+        if (isLocked)
+        {
+            if (Managers.Inventory.GetItemCount(requiredItem.gameObject.name) >= requiredItemCount)
+            {
+                for (int i = 0; i < requiredItemCount; i++)
+                {
+                    Managers.Inventory.ConsumeItem(requiredItem.gameObject.name);
+                }
+                StartCoroutine(ToggleDoor());
+                isLocked = false;
+            } else
+            {
+                return;
+            }
+        }
+        else
+        {
+            StartCoroutine(ToggleDoor());
+        }
+        
+        
     }
 
     private IEnumerator ToggleDoor()
