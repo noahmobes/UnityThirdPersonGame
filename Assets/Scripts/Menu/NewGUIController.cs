5ff045d353f97037ab598ed5438127de8c61bc1b
noahmobes <noahmobes@outlook.com>
2026-08-01 14:23:40 -0600

    Added eventsystem to make canvas buttons work

--- Assets/Scripts/Menu/NewGUIController.cs
+++ Assets/Scripts/Menu/NewGUIController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class GameGuiController : MonoBehaviour
+public class NewGameGuiController : MonoBehaviour
 {
     [Header("Scene Manager")]
     public SceneLoader sceneManager;
